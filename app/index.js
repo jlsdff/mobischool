eac8fbe28d87c2f72c63a863ab5a463151127aef
jlsdff <8215455@ntc.edu.ph>
2023-12-08 05:17:44 +0800

    seperator added

--- app/index.js
+++ app/index.js
@@ -1,18 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native"
-import { Stack } from "expo-router";
+import { View, Text, TextInput, ImageBackground, Image } from "react-native"
+import { Stack, Link } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
-import { styled } from "nativewind";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Seperator from "../components/seperator";
+
+const background = { uri: "../assets/background.png" }
 
 export default function Index() {
     return (
-        <View className="flex-1 bg-slate-100">
+        <View className="flex-1 bg-slate-900 ">
             <StatusBar style="dark" />
             <Stack.Screen options={{
-                // headerShown: false
-                // headerTransparent: true
-            }} /> 
-            <Text className="text-orange-950 text-3xl" >Hello World!</Text>
+                headerShown: false
+            }} />
+            <ImageBackground className="flex-1" source={require("../assets/background.png")} resizeMode="cover" >
+                < Image className="h-[200px] w-[200px] mx-auto mt-14" source={require("../assets/icon.png")} />
+                <View className="gap-4 px-4">
+                    <Text className="text-slate-950 text-3xl text-center text-black font-sans" >Sign in</Text>
+                    <TextInput
+                        className="h-13 py-2 px-4 border-2 rounded-xl border-neutral7 "
+                        placeholder="Email"
+                    />
+                    <TextInput
+                        className="h-13 py-2 px-4 border-2 rounded-xl border-neutral7 "
+                        placeholder="Password"
+                        secureTextEntry={true}
+                    />
+                    <View className="flex-row justify-between items-center">
+                        <BouncyCheckbox
+                            onPress={() => { }}
+                            text="remember me?"
+                            fillColor="#495057"
+                            textStyle={{
+                                textDecorationLine: "none",
+                            }}
+                        />
+                        <Link href="forgot"  >Forgot Password?</Link>
+                    </View>
+                    <TouchableOpacity className="border border-1 border-slate-900 rounded-xl h-13 py-3 px-5 bg-primary-dark" >
+                        <Text className="text-center text-xl text-txt-light2">Sign in</Text>
+                    </TouchableOpacity>
+
+                    {/* Seperator */}
+                    <View className="flex-row justify-center items-center" >
+                        <View className="flex-1 h-[1px] bg-neutral6" />
+                        <View>
+                            <Text className="w-[50] text-center" >OR</Text>
+                        </View>
+                        <View className="flex-1 h-[1px] bg-neutral6" />
+                    </View>
+
+                    
+                    
+                </View>
+            </ImageBackground>
         </View>
     )
 }
